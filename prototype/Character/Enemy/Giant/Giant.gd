4cdbdda8335978ef574e2303289d52c5d69d24ad
ChemicalInk <38759407+ChemicalInk@users.noreply.github.com>
2018-06-19 22:17:27 +0300

    Restructured giant, redid some things in a more sane way

--- prototype/Character/Enemy/Giant/Giant.gd
+++ prototype/Character/Enemy/Giant/Giant.gd
@@ -1,7 +1,5 @@
-extends "Enemy.gd"
+extends "../Enemy.gd"
 
 func _ready():
-	self.max_health = 30
-	self.health = self.max_health
 	self._unregister_damaging_group("sword") # TODO: add hitboxes or parts
 	print("Giant reporting.")
