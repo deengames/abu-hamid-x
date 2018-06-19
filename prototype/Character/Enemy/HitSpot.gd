0f34a441050f7d157a94122e2548cf1c5e6594e7
nightblade9 <nightbladecodes@gmail.com>
2018-06-19 16:24:50 -0400

    Hit collision on hit spots works

--- prototype/Character/Enemy/HitSpot.gd
+++ prototype/Character/Enemy/HitSpot.gd
@@ -7,9 +7,19 @@ extends "../Character.gd"
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
+	print("Registerinmg")
 	self.register_damaging_group("sword")
+	print("Registered")
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
+
+
+func _on_HitSpot_damaged():
+	print("DAMAGED")
+
+
+func _on_HitSpot_body_entered(body):
+	print("test")
