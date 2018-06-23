ae35365c52eda502b8ff62c4e60b810d9989be1d
ChemicalInk <38759407+ChemicalInk@users.noreply.github.com>
2018-06-23 21:39:07 +0300

    Implemented wave algorithm

--- prototype/Main.gd
+++ prototype/Main.gd
@@ -1,29 +1,42 @@
 extends Node2D
 
 export (int) var waves_per_game = 10
-export (int) var enemies_in_first_wave = 3
-export (int) var increment_per_wave = 1
+export (int) var points_in_first_wave = 1
+export (int) var increment_per_wave = 2
 
+var enemies_to_spawn = {
+	preload("res://prototype/Character/Enemy/Enemy.tscn"): 1,
+	preload("res://prototype/Character/Enemy/Giant/Giant.tscn"): 5
+}
 
-var scenes_to_spawn = [preload("res://prototype/Character/Enemy/Enemy.tscn")]
 var wave_num = 0
 var num_spawned_entities = 0
+var carry_over_points = 0
 onready var timer = $WaveSpawnTimer
-
-var Giant_tscn = preload("res://prototype/Character/Enemy/Giant/Giant.tscn")
 
 
 func _new_wave():
-#	var total_enemies = enemies_in_first_wave + increment_per_wave * wave_num
-#	wave_num += 1
-#	for i in range(total_enemies):
-#		var scene = scenes_to_spawn[randi() % len(scenes_to_spawn)]
-#		var e = scene.instance()
-#		add_child(e)
-#		e.connect('death', self, '_on_spawned_entity_death')
-#		num_spawned_entities += 1
-	
-	self._spawn_giant()
+	wave_num += 1
+	var points_this_wave = points_in_first_wave + (increment_per_wave * wave_num) + carry_over_points
+	carry_over_points = 0
+	while points_this_wave > 0:
+		var random_enemy = enemies_to_spawn.keys()[randi() % len(enemies_to_spawn)]
+		var price = enemies_to_spawn[random_enemy]
+		if price <= points_this_wave:
+			points_this_wave -= price
+			var e = random_enemy.instance()
+			add_child(e)
+			e.connect('death', self, '_on_spawned_entity_death')
+			num_spawned_entities += 1
+		else:  # exit if there isn't at least one affordable entity
+			var lowest_price = INF
+			for p in enemies_to_spawn.values():
+				if p < lowest_price:
+					lowest_price = p
+			if points_this_wave < lowest_price:
+				carry_over_points = points_this_wave
+				break
+
 
 func _on_spawned_entity_death():
 	num_spawned_entities -= 1
@@ -33,8 +46,3 @@ func _process(delta):
 	if num_spawned_entities <= 0 and timer.is_stopped():
 		timer.start()
 	$ui/WaveLabel.text = "WAVE: " + str(wave_num) + ' / ' + str(waves_per_game)
-
-func _spawn_giant():
-	var e = Giant_tscn.instance()
-	add_child(e)
-	num_spawned_entities = 1 # keep it from constantly spawning
