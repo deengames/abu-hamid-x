192c561f4c4c09440ef73bb135bbc8dfbe069e76
ChemicalInk <aladdinalkhafaji@gmail.com>
2018-07-25 22:34:32 +0300

    PowerupSpawn spawns powerups according to enemy deaths

--- src/prototype/PowerupSpawn/PowerupSpawn.gd
+++ src/prototype/PowerupSpawn/PowerupSpawn.gd
@@ -1,22 +1,20 @@
 extends Area2D
 
-export (int) var seconds_per_powerup = 12
+export (float) var powerups_per_enemy_death = 0.2
 
 var powerup_cls
 onready var shape = $CollisionShape2D.shape
 
-var seconds_since_powerup_spawn = 999
-
-func _process(delta):
-	seconds_since_powerup_spawn += delta
-	var x = shape.extents.x
-	var y = shape.extents.y
-	
-	var spawn_location = Vector2(
-		rand_range(-x, x),
-		rand_range(-y, y)
-	)
-	
-	var powerup = powerup_cls.instance()
-	powerup.init(spawn_location)
-	add_child(powerup)
+func _on_enemy_death(enemy):
+	if randf() > powerups_per_enemy_death:
+		var x = shape.extents.x
+		var y = shape.extents.y
+		
+		var spawn_location = Vector2(
+			rand_range(-x, x),
+			rand_range(-y, y)
+		)
+		
+		var powerup = powerup_cls.instance()
+		powerup.init(spawn_location)
+		add_child(powerup)
