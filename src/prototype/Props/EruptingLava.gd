1e8cc7543a14a32fd12d4c1f2ac501a93799ecca
ChemicalInk <aladdinalkhafaji@gmail.com>
2018-07-29 23:35:08 +0300

    Implement random eruptions

--- src/prototype/Props/EruptingLava.gd
+++ src/prototype/Props/EruptingLava.gd
@@ -1,6 +1,6 @@
 extends "Lava.gd"
 
-export (float) var movement_speed = 400
+export (float) var movement_speed = 300
 
 signal relocate(thing)
 
@@ -10,6 +10,9 @@ func _ready():
 	var notifier = $VisibilityNotifier2D
 	while notifier.is_on_screen():
 		emit_signal("relocate", self)
+
+func init(pos):
+	position = pos
 
 func _physics_process(delta):
 	position += velocity * delta
