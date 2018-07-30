1bcbd18879a6c76c6b1448658ba43d8d889f670b
ChemicalInk <aladdinalkhafaji@gmail.com>
2018-07-30 19:16:00 +0300

    Implemented assassin enemies

--- src/prototype/Character/Enemy/Assassin/Assassin.gd
+++ src/prototype/Character/Enemy/Assassin/Assassin.gd
@@ -1,26 +1,17 @@
 extends "../Enemy.gd"
 
-export (float) var lunge_attack_range = 250
+export (float) var lunge_attack_range = 200
 export (float) var lunge_attack_force = 2000
-export (float) var seconds_visible = 3
+export (float) var lunge_cool_down = 2
 
-onready var appear_tween = $AppearTween
-onready var disappear_tween = $DisappearTween
-
-func _ready():
-	$VisibleTimer.wait_time = seconds_visible
+var seconds_since_last_lunge = 999
 
 func _integrate_forces(state):
-	if player.position.distance_to(position) > lunge_attack_range:
-		._integrate_forces(state)
-	else:
-		var angle_to_player = player.position.angle_to(position)
-		var lunge_vector = Vector2(0, lunge_attack_force).rotated(angle_to_player)
-		appear_tween.interpolate_property($ColorBox.color, "a", 0.07, 1, 0.5, Tween.TRANS_LINEAR)
+	seconds_since_last_lunge += state.step
+	if player.position.distance_to(position) < lunge_attack_range and seconds_since_last_lunge > lunge_cool_down:
+		seconds_since_last_lunge = 0
+		var angle_to_player = player.position.angle_to_point(position) + PI/2
+		var lunge_vector = Vector2(0, -lunge_attack_force).rotated(angle_to_player)
 		apply_impulse(Vector2(0, 0), lunge_vector)
-
-func _on_AppearTween_tween_completed(object, key):
-	$VisibleTimer.start()
-
-func _on_VisibleTimer_timeout():
-	disappear_tween.interpolate_property($ColorBox.color, "a", 1, 0.07, 1.5, Tween.TRANS_LINEAR)
+	else:
+		._integrate_forces(state)
--- src/prototype/ScenarioSelect.gd
+++ src/prototype/ScenarioSelect.gd
@@ -21,7 +21,8 @@ func _on_Button2_pressed():
 	global.current_spawns = {
 		preload("res://prototype/Character/Enemy/Enemy.tscn"): 1,
 		preload("res://prototype/Character/Enemy/ShooterEnemy.tscn"): 3,
-		preload("res://prototype/Character/Enemy/Kamikaze/Kamikaze.tscn"): 2
+		preload("res://prototype/Character/Enemy/Kamikaze/Kamikaze.tscn"): 2,
+		preload("res://prototype/Character/Enemy/Assassin/Assassin.tscn"): 2
 	}
 	
 	get_tree().change_scene('res://prototype/Scenario2.tscn')
