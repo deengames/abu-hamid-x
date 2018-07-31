4ea824a0a82d7f95d8be4449a1ca3d1dc591cf67
ChemicalInk <aladdinalkhafaji@gmail.com>
2018-07-31 22:01:03 +0300

    Balance enemy costs

--- src/prototype/ScenarioSelect.gd
+++ src/prototype/ScenarioSelect.gd
@@ -4,7 +4,7 @@ extends CanvasLayer
 func _on_Level1Button_pressed():
 	global.current_spawns = {
 		preload("res://prototype/Character/Enemy/Enemy.tscn"): 1,
-		preload("res://prototype/Character/Enemy/ShooterEnemy.tscn"): 3
+		preload("res://prototype/Character/Enemy/ShooterEnemy.tscn"): 2
 	}
 	
 	global.special_spawns = {
@@ -17,7 +17,7 @@ func _on_Level1Button_pressed():
 func _on_Level2BUtton_pressed():
 	global.current_spawns = {
 		preload("res://prototype/Character/Enemy/Enemy.tscn"): 1,
-		preload("res://prototype/Character/Enemy/ShooterEnemy.tscn"): 3,
+		preload("res://prototype/Character/Enemy/ShooterEnemy.tscn"): 2,
 		preload("res://prototype/Character/Enemy/Kamikaze/Kamikaze.tscn"): 2
 	}
 	global.special_spawns = {}
@@ -27,7 +27,7 @@ func _on_Level2BUtton_pressed():
 func _on_Level3Button_pressed():
 	global.current_spawns = {
 		preload("res://prototype/Character/Enemy/Enemy.tscn"): 1,
-		preload("res://prototype/Character/Enemy/ShooterEnemy.tscn"): 3,
+		preload("res://prototype/Character/Enemy/ShooterEnemy.tscn"): 2,
 		preload("res://prototype/Character/Enemy/Kamikaze/Kamikaze.tscn"): 2
 	}
 	global.special_spawns = {}
@@ -37,9 +37,9 @@ func _on_Level3Button_pressed():
 func _on_Level4Button_pressed():
 	global.current_spawns = {
 		preload("res://prototype/Character/Enemy/Enemy.tscn"): 1,
-		preload("res://prototype/Character/Enemy/ShooterEnemy.tscn"): 3,
+		preload("res://prototype/Character/Enemy/ShooterEnemy.tscn"): 2,
 		preload("res://prototype/Character/Enemy/Kamikaze/Kamikaze.tscn"): 2,
-		preload("res://prototype/Character/Enemy/Assassin/Assassin.tscn"): 2
+		preload("res://prototype/Character/Enemy/Assassin/Assassin.tscn"): 3
 	}
 	global.special_spawns = {}
 	
