a4d7a775821a9f6a14c33f96f87bc1998d6b3642
ChemicalInk <aladdinalkhafaji@gmail.com>
2018-08-08 01:12:40 +0300

    Unit tests for ControlledHorizontalMovement component

--- tests/component/movement/test_ControlledHorizontalMovement.gd
+++ tests/component/movement/test_ControlledHorizontalMovement.gd
@@ -2,6 +2,44 @@ extends "res://addons/gut/test.gd"
 
 var component_cls = preload('res://src/component/movement/ControlledHorizontalMovement.gd')
 
+var velocity = Vector2()
+var comp
+
+func setup():
+	comp = component_cls.new()
+	add_child(comp)
+
+func teardown():
+	remove_child(comp)
+
 func test_init():
-    var comp = component_cls.new()
-    assert_true(comp != null)
+	assert_true(comp != null)
+
+func test_physics_process_does_nothing_if_no_input():
+	var old_velocity = velocity
+
+	comp._physics_process(0)
+
+	assert_eq(velocity, old_velocity)
+
+
+func test_physics_process_moves_left_if_input_left():
+	var old_velocity = velocity
+	Input.action_press('move_left')
+
+	comp._physics_process(0)
+
+	assert_lt(velocity, old_velocity)
+
+	Input.action_release('move_left')
+
+
+func test_physics_process_moves_right_if_input_right():
+	var old_velocity = velocity
+	Input.action_press('move_right')
+
+	comp._physics_process(0)
+
+	assert_gt(velocity, old_velocity)
+
+	Input.action_release('move_right')
