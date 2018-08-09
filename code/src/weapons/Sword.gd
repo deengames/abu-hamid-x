3d040644913f809368ef7474f2d33ef636973310
ChemicalInk <aladdinalkhafaji@gmail.com>
2018-08-09 12:16:20 +0300

    Refactored sword.gd and wrote more useful tests

--- code/src/weapons/Sword.gd
+++ code/src/weapons/Sword.gd
@@ -18,29 +18,36 @@ func _process(delta):
 		swing()
 
 
-func _get_swing_direction():
-	return DIRECTION.LEFT if get_local_mouse_position().x < 0 else DIRECTION.RIGHT
+func _get_swing_direction(mouse_pos):
+	return DIRECTION.LEFT if global_position.x > mouse_pos.x else DIRECTION.RIGHT
+
+func _process_swing(mouse_pos):
+	# minus PI/2 for correction: needed because 
+	# angle_to_point gets angle relative to `x` axis
+	# while rotation is relative to `y` axis
+	# that is, if rotation == 0, object points towards -y
+	var angle_to_mouse = global_position.angle_to_point(mouse_pos) - PI/2
+	
+	var swing_range
+	if _get_swing_direction(mouse_pos) == DIRECTION.LEFT:
+		swing_range = deg2rad(swing_angle_degrees)
+	else:
+		swing_range = -deg2rad(swing_angle_degrees)
+	
+	var starting_angle = angle_to_mouse - swing_range
+	var ending_angle = angle_to_mouse + swing_range
+	
+	return {start=starting_angle, end=ending_angle}
 
 func swing():
 	if not is_swinging:
 		is_swinging = true
-		
-		# minus PI/2 for correction
-		# there's probably a non-correction-needy way to do it
-		# but nothing else I've tried works, soooo
-		var angle_to_mouse = get_parent().position.angle_to_point(get_global_mouse_position()) - PI/2
-		
-		var swing_range
-		if _get_swing_direction() == DIRECTION.LEFT:
-			swing_range = deg2rad(swing_angle_degrees)
-		else:
-			swing_range = -deg2rad(swing_angle_degrees)
-		
-		var starting_angle = angle_to_mouse - swing_range
-		var ending_angle = angle_to_mouse + swing_range
+	
+		var m = get_global_mouse_position()
+		var swing_angle = _process_swing(m)
 		
 		tween.interpolate_property(
-				self, 'rotation', starting_angle, ending_angle,
+				self, 'rotation', swing_angle.start, swing_angle.end,
 				seconds_per_swing, Tween.TRANS_LINEAR, Tween.EASE_IN)
 		tween.start()
 
