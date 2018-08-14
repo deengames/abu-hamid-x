4e2605e71327fc24194a6c193e287b44d83b79b8
ChemicalInk <aladdinalkhafaji@gmail.com>
2018-08-14 20:26:42 +0300

    Guns duplicate prototype bullets

--- code/src/weapons/Gun.gd
+++ code/src/weapons/Gun.gd
@@ -5,12 +5,15 @@ signal bullet_shot(bullet)
 export (float) var bullet_speed = 1500
 export (float) var bullets_per_second = 3.0
 
-var bullet_cls = preload("res://src/weapons/Bullet/Bullet.tscn")
+onready var bullet_prototype = $PrototypeBullet
+
+func _ready():
+	remove_child(bullet_prototype)
 
 var seconds_since_last_shot = 999
 
 func _create_bullet(mouse_pos):
-	var bullet = bullet_cls.instance()
+	var bullet = bullet_prototype.duplicate()
 	
 	# minus PI/2 for correction
 	var shoot_angle = global_position.angle_to_point(mouse_pos) - PI/2
