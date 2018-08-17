6e012534ce500d9a8ea06a4d3a5a6995d4d23035
ChemicalInk <aladdinalkhafaji@gmail.com>
2018-08-17 22:14:32 +0300

    Wiring on ShooterEnemy

--- code/src/actor/ShooterEnemy.gd
+++ code/src/actor/ShooterEnemy.gd
@@ -2,5 +2,9 @@ extends KinematicBody2D
 
 var velocity = Vector2()
 
+func init(player):
+	$FollowsEntity.init(player)
+	$ShootsEntity.init(player)
+
 func _physics_process(delta):
 	velocity = move_and_slide(velocity, Vector2(0, -1))
