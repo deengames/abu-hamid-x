a33cf0d6bb65e1c2c45eadc94b323de56e666eed
nightblade9 <nightbladecodes@gmail.com>
2018-08-23 18:47:16 -0400

    Enemies can be hit (collision layers) Made enemies RigidBody2D so physics applies to them

--- code/src/actor/ShooterEnemy.gd
+++ code/src/actor/ShooterEnemy.gd
@@ -1,10 +1,19 @@
-extends KinematicBody2D
+extends RigidBody2D
 
 var velocity = Vector2()
+var player
 
 func init(player):
+	self.player = player
 	$FollowsEntity.init(player)
 	$ShootsEntity.init(player)
 
-func _physics_process(delta):
-	velocity = move_and_slide(velocity, Vector2(0, -1))
+func _integrate_forces(state):
+	var velocity = state.get_linear_velocity()
+	var movement_speed = 1
+	var gained_velocity = Vector2(-movement_speed, 0).rotated(position.angle_to_point(self.player.position))
+	velocity += gained_velocity
+	
+	velocity = Vector2(lerp(velocity.x, 0, 0.05), lerp(velocity.y, 0, 0.05))
+	velocity += state.get_total_gravity() * state.step
+	state.set_linear_velocity(velocity)
